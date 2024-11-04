b2bd866c7d56389b1ed5b96797d1f4510a84a9b8
VicMarBall <99729766+VicMarBall@users.noreply.github.com>
2024-11-04 09:43:09 +0100

    player wip stuff

--- Light & Dark/Assets/Scripts/Gameplay/PlayerController.cs
+++ Light & Dark/Assets/Scripts/Gameplay/PlayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    Rigidbody rigidBody;
+
+	// Start is called before the first frame update
+	void Start()
     {
         
     }
@@ -13,6 +16,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckMovement();
     }
+
+    void CheckMovement()
+    {
+        Vector3 velocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity += transform.rotation * Vector3.forward;
+        }
+		if (Input.GetKey(KeyCode.A))
+		{
+			velocity += transform.rotation * Vector3.left;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			velocity += transform.rotation * Vector3.back;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			velocity += transform.rotation * Vector3.right;
+		}
+
+		velocity.Normalize();
+
+        if (velocity != Vector3.zero)
+        {
+			rigidBody.velocity = velocity;
+		}
+	}
 }
